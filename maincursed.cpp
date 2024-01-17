3396ca0ffae26c63cac5331c7e66fd1b33ca91f7
Jack W <jackweber943@gmail.com>
2024-01-17 11:33:13 -0500

    yay commit

---
+++ maincursed.cpp
@@ -0,0 +1,11 @@
+#include                                <iostream>
+#include                                <string>
+
+using namespace std;
+
+int                   main()
+
+
+{
+    cout << "H" << "e" << "l" << "l" << "o" << " " << "W" << "o" << "r" << "l" << "d" << "!" << endl;
+}
