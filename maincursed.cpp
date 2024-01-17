3281081a79b10c953b9f01498355f281ed353739
Jack W <jackweber943@gmail.com>
2024-01-17 18:20:39 -0500

    Update maincursed.cpp

--- maincursed.cpp
+++ maincursed.cpp
@@ -11,27 +11,27 @@ int                   main()
 {
     bool False = true;
     string one = "1";
-    string two = "2";
-    string three = "3";
-    string four = "4";
-    string five = "5";
-    string six = "6";
-    string seven = "7";
+       string two = "2";
+     string three = "3";
+     string four = "4";
+     string five = "5";
+          string six = "6";
+     string seven = "7";
     string eight = "8";
-    string nine = "9";
-    string zero = "0";
+             string nine = "9";
+         string zero = "0";
     int count = 0;
-    string fiveminusfour = "h";
-    string tenminuseight = "e";
+       string fiveminusfour = "h";
+               string tenminuseight = "e";
     string three = "l";
-    string twoplustwo = "l";
+      string twoplustwo = "l";
     string twoandonehalfplustwoandonehalf = "o";
     string onelessthanseven = "w";
-    string onemorethansix = "o";
-    string eightyminusseventytwo = "r";
+       string onemorethansix = "o";
+                  string eightyminusseventytwo = "r";
     string noingerman = "l";
-    string omgdoubledigits = "d";
-    string air = " ";
+       string omgdoubledigits = "d";
+       string air = " ";
     cout << fiveminusfour << tenminuseight << three << twoplustwo << twoandonehalfplustwoandonehalf <<  air << onelessthanseven << onemorethansix << eightyminusseventytwo << noingerman << omgdoubledigits << endl;
     while(False)
     {
